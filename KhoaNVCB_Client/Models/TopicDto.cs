943444a98a46faf983ac866f1512efe016111f31
DatBui1 <buiphatdat815@gmail.com>
2026-04-06 13:47:39 +0700

    bigup

---
+++ KhoaNVCB_Client/Models/TopicDto.cs
@@ -0,0 +1,14 @@
+﻿namespace KhoaNVCB_Client.Models // Thay đổi theo namespace của bạn
+{
+    public class TopicDto
+    {
+        public int TopicId { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string? FileUrl { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Pending";
+        public string? UserId { get; set; }
+        public string? AuthorName { get; set; }
+    }
+}
